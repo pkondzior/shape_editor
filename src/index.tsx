7b4b0444712602d5543eadad8f4f4aac6b100630
Pawel Kondzior <pawel@kondzior.com>
2016-07-23 20:25:08 -0700

    Initial commit with typescript bootstraped app

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { App } from "./components/app";
+import {render} from "react-dom";
+
+render(<App compiler="TypeScript" framework="React" />, document.body);
+
